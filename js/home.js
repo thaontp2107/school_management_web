84da888ec4181e83d001ad6f8a3d48e4d4e4ec6b
thaontp2107 <thaontp2107@gmail.com>
2026-01-25 19:45:25 +0700

    tao danh sach
    -tao danh sach

--- js/home.js
+++ js/home.js
@@ -45,6 +45,47 @@ function loadPage(url) {
         });
 }
 
+
+function showStudentList() {
+    const box = document.getElementById("contentBox");
+
+    const students = [
+        { id: "2222", name: "Phương Thảo", class: "Grade 06", dob: "08/05/2007" },
+        { id: "2223", name: "Nguyễn Văn An", class: "Grade 07", dob: "12/09/2007" },
+        { id: "2224", name: "Doan Thi Anh", class: "Grade 08", dob: "02/05/2007" },
+        { id: "2225", name: "Nguyễn Phuong", class: "Grade 09", dob: "21/09/2007" },
+        { id: "2226", name: "Tran Văn HUng", class: "Grade 10", dob: "01/09/2007" },
+        { id: "2227", name: "Nguyễn Cam Van", class: "Grade 13", dob: "18/08/2007" },
+        { id: "2228", name: "Le Văn B", class: "Grade 02", dob: "22/07/2007" },
+        { id: "2229", name: "Phan A", class: "Grade 01", dob: "05/09/2007" },
+        { id: "2230", name: "Nguyễn Nguyen", class: "Grade 05", dob: "09/12/2007" },
+        { id: "2231", name: "Tran Thi Ly", class: "Grade 11", dob: "17/10/2007" }
+    ];
+
+    let html = `
+    <div class="student-box">
+        <div class="student-header">
+            <span class=>Mã số</span>
+            <span>Họ tên</span>
+            <span>Lớp</span>
+            <span>Ngày sinh</span>
+        </div>
+    `;
+
+    students.forEach(s => {
+        html += `
+        <div class="student-row">
+            <span>${s.id}</span>
+            <span>${s.name}</span>
+            <span>${s.class}</span>
+            <span>${s.dob}</span>
+        </div>
+        `;
+    });
+
+    html += `</div>`;
+    box.innerHTML = html;
+}
 document.addEventListener("DOMContentLoaded", function () {
     const firstMenu = document.querySelector(".menu li");
     if (firstMenu) {
