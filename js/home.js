e3d0def4c581632891ee1587f64c8c4f533eb4da
thaontp2107 <thaontp2107@gmail.com>
2026-01-25 19:45:25 +0700

    tao danh sach
    -tao danh sach

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@ function showContent(section) {
         box.innerText = "Nội dung Dashboard";
     } else if (section === "students") {
         title.innerText = "Students";
-        box.innerText = "Danh sách sinh viên";
+        showStudentList();  
     } else if (section === "staff") {
         title.innerText = "Academic Staff";
         box.innerText = "Danh sách giảng viên";
@@ -32,16 +32,56 @@ function changePage(menu, element) {
     // Gán active cho mục đang bấm
     element.classList.add("active");
 
-    // // Đổi tiêu đề & nội dung
-    document.getElementById("title").innerText = menu;
-    document.getElementById("contentBox").innerText = "Nội dung " + menu;
+    // Gọi showContent để xử lý nội dung
+    showContent(menu);
 }
 
+
+function showStudentList() {
+    const box = document.getElementById("contentBox");
+
+    const students = [
+        { id: "2222", name: "Phương Thảo", class: "Grade 06", dob: "08/05/2007" },
+        { id: "2223", name: "Nguyễn Văn An", class: "Grade 07", dob: "12/09/2007" },
+        { id: "2224", name: "Doan Thi Anh", class: "Grade 08", dob: "02/05/2007" },
+        { id: "2225", name: "Nguyễn Phuong", class: "Grade 09", dob: "21/09/2007" },
+        { id: "2226", name: "Tran Văn HUng", class: "Grade 10", dob: "01/09/2007" },
+        { id: "2227", name: "Nguyễn Cam Van", class: "Grade 13", dob: "18/08/2007" },
+        { id: "2228", name: "Le Văn B", class: "Grade 02", dob: "22/07/2007" },
+        { id: "2229", name: "Phan A", class: "Grade 01", dob: "05/09/2007" },
+        { id: "2230", name: "Nguyễn Nguyen", class: "Grade 05", dob: "09/12/2007" },
+        { id: "2231", name: "Tran Thi Ly", class: "Grade 11", dob: "17/10/2007" }
+    ];
+
+    let html = `
+    <div class="student-box">
+        <div class="student-header">
+            <span class=>Mã số</span>
+            <span>Họ tên</span>
+            <span>Lớp</span>
+            <span>Ngày sinh</span>
+        </div>
+    `;
+
+    students.forEach(s => {
+        html += `
+        <div class="student-row">
+            <span>${s.id}</span>
+            <span>${s.name}</span>
+            <span>${s.class}</span>
+            <span>${s.dob}</span>
+        </div>
+        `;
+    });
+
+    html += `</div>`;
+    box.innerHTML = html;
+}
 document.addEventListener("DOMContentLoaded", function () {
     const firstMenu = document.querySelector(".menu li");
     if (firstMenu) {
         firstMenu.classList.add("active");
-        changePage("Dashboard", firstMenu);
+        showContent("dashboard"); // 👈 đúng tên section
     }
 });
 
@@ -51,3 +91,4 @@ function toggleMenu() {
     sidebar.style.display =
         sidebar.style.display === "none" ? "block" : "none";
 }
+
